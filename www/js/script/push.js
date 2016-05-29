3d2f5a416e8ff6dd82d8321e09a05735949b0eed
atriatech94 <hamid_7555@hotmail.com>
2016-05-29 15:14:18 +0430

    n33

--- www/js/script/push.js
+++ www/js/script/push.js
@@ -17,35 +17,49 @@ var app1 = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
        
-      push = PushNotification.init({
-            android: {
-                senderID: "12345679"
-            },
-            ios: {
-                alert: "true",
-                badge: "true",
-                sound: "true"
-            },
-            windows: {}
-        });
-
-        push.on('registration', function(data) {
-            alert (data.registrationId)
-        });
-
-        push.on('notification', function(data) {
-            // data.message,
-            // data.title,
-            // data.count,
-            // data.sound,
-            // data.image,
-            // data.additionalData
-        });
-
-        push.on('error', function(e) {
-            // e.message
-        });
+    
+       initPushwoosh();
+       document.addEventListener('push-notification', function(event) {
+             var notification = event.notification;
+             alert(notification.aps.alert);
+             pushNotification.setApplicationIconBadgeNumber(0);
+       });
+           
        
     }
 };
 app1.initialize();
+
+function initPushwoosh() {
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+ 
+    //set push notification callback before we initialize the plugin
+    document.addEventListener('push-notification', function(event) {
+                                //get the notification payload
+                                var notification = event.notification;
+ 
+                                //display alert to the user for example
+                                alert(notification.aps.alert);
+                               
+                                //clear the app badge
+                                pushNotification.setApplicationIconBadgeNumber(0);
+                            });
+ 
+    //initialize the plugin
+    pushNotification.onDeviceReady({pw_appid:"P4DF84-87724"});
+     
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var deviceToken = status['deviceToken'];
+            console.warn('registerDevice: ' + deviceToken);
+        },
+        function(status) {
+            console.warn('failed to register : ' + JSON.stringify(status));
+            alert(JSON.stringify(['failed to register ', status]));
+        }
+    );
+     
+    //reset badges on app start
+    pushNotification.setApplicationIconBadgeNumber(0);
+}
