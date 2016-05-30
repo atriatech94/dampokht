d35c27fcf4aa4ae7b5e8c7bcbed10cb56dd90fe5
atriatech94 <hamid_7555@hotmail.com>
2016-05-30 14:45:12 +0430

    n55

--- js/controller/home-controller.js
+++ js/controller/home-controller.js
@@ -27,33 +27,7 @@ angular.module('my-app')
         $scope.go = function(path){
             $location.path(path);
         }
-        
-            /* register for push */
-            if(localStorage.getItem('reg_id') && !localStorage.getItem('has_reg_id')){
-                    $http({
-                    method: 'POST',
-                    url: base_url+'reg_id/HDaMin2dsaZ3QZYTRRE782',
-                    data: $.param({token_id: localStorage.getItem('reg_id') , user_id : localStorage.getItem('user_id'), type : 2}),
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                }).then(function successCallback(response) {
-                    localStorage.setItem('has_reg_id',1);  
-                });   
-                }
-                else if(!localStorage.getItem('reg_id') && !localStorage.getItem('has_reg_id')) {
-                    push.on('registration', function(data) {
-                        localStorage.setItem('reg_id',data.registrationId);
-                        $http({
-                            method: 'POST',
-                            url: base_url+'reg_id/HDaMin2dsaZ3QZYTRRE782',
-                            data: $.param({token_id: localStorage.getItem('reg_id') , user_id : localStorage.getItem('user_id') , type : 2}),
-                            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                        }).then(function successCallback(response) {
-                            localStorage.setItem('has_reg_id',1);    
-                        });
-                        
-                    });
-            }
-            /* end register for push */
+       
    
 })
 
