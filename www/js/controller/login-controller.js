256f127b0032abe3217bf8561c9e0195e35d25d6
atriatech94 <hamid_7555@hotmail.com>
2016-07-11 14:02:29 +0430

    n56

--- www/js/controller/login-controller.js
+++ www/js/controller/login-controller.js
@@ -178,7 +178,12 @@ angular.module('my-app')
 .controller('verifyController', function($scope,$location,$http) {
       $scope.forgot_user_id = localStorage.getItem('forgot_user_id');
       $scope.go = function ( path ) {$location.path( path )};
-      smsplugin.startReception(successCallback(result),failureCallback(error));
+       var smsInboxPlugin = cordova.require('cordova/plugin/smsinboxplugin');
+       smsInboxPlugin.startReception (function(msg) {
+            alert(msg);
+        }, function() {
+            alert("Error while receiving messages");
+        });
       $scope.submit = function () {
          $scope.user = document.getElementById('username').value;
          $scope.password = document.getElementById('password').value;
